c8372eab2391c77c20f5139c117fc73e4d5f2133
Makhlug Jafarov <makhlugjafarov@gmail.com>
2022-02-16 23:14:48 +0100

    added brute force attack dictionary code

---
+++ ceaser_bruto_force_attack/README.md
@@ -0,0 +1,5 @@
+## Write python code that does a brute force attack against Caesar encryption.
+
+# Get a simple textfile containing a few thousand common English words.
+# Create a program that takes in an encoded string, then try decoding various messages of ALL possibilities
+# Use the dictionary to try to automatically determine which shift is most likely be a valid plaintext.
---
+++ ceaser_bruto_force_attack/ceaser_brute_force_attack_dictionary.py
@@ -0,0 +1,83 @@
+import string, random
+
+def chooseRandomWordFromDict(fileName):
+    #open the file
+    words = open(fileName)
+
+    #choose the word (line) to be encrypted randomly
+    wordIndex = random.randint(0, 1276) #there are 370103 words in the file
+
+    for lineNum, line in enumerate(words):
+        if (lineNum == wordIndex):
+            return line
+    words.close()
+
+#function definition for encryption
+def encrypt (inputText, key):
+    '''
+    The function gets a text as a string
+    and a key. 
+    The function returns the encrypted text.
+    '''
+    encrypted = ""
+    for i in range (len(inputText)):
+        ch = inputText[i]
+        if (ch.isupper()):
+            encrypted += chr((ord(ch) + key - 65) % 26 + 65)
+        else:
+            encrypted += chr((ord(ch)+ key - 97) % 26 + 97)
+    return encrypted
+
+def bruteForceGuess(fileName, chiperText):
+    #create a list of English Lowercase Letters
+    letters = string.ascii_lowercase
+
+    #iterate over the indices of each letter of letters
+    for key in range(len(letters)): # 0 - 25
+        guess = ''
+        #iterate over each characters of the encrypted word
+        for ch in chiperText:
+            if ch in letters: #if the character is lowercase English word
+                index = letters.find(ch) #find the index of the character in the list
+                #find the encrypted (guessed) character
+                index = (index - key) % 26
+                guess = guess + letters[index]
+            else:
+                guess = guess + ch
+        #open the file
+        words = open(fileName)
+        #search for the guessed word in the file
+        for line in enumerate(words):
+            word = line[1].strip()
+            if (guess == word):
+                print("Guessed word is \"", guess, "\". Key is ", key, ".")
+                return key
+        #close the file
+        words.close()
+
+#######################################################
+
+#open the file
+fileName = "words.txt"
+
+#choose the word randomly from the file
+randomWord = chooseRandomWordFromDict(fileName)
+randomWord = randomWord.strip() #remove the newline character
+print("Random word: ", randomWord, "\n")
+
+#the key value is randomly found
+#it will be sent to the receiver in other means of contacting
+key = random.randint(0, 25) #between 0 and 25
+
+#encrypt the input text with the defined key
+#the encrypted text is assigned to the new variable
+encryptedText = encrypt(randomWord, key)
+print("encrypted: ", encryptedText, "\n")
+
+#call the brute force attack
+foundKey = bruteForceGuess(fileName, encryptedText)
+print()
+if (foundKey == key): #if the guessed key is correct
+    print("SUCCESFUL!")
+else:
+    print("UNSUCCESFUL!")
